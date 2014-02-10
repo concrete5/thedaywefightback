5356fb56a6039a4cf7909094cf309d1401d404fd
tylerryan <ryan@concrete5.org>
2014-02-10 11:11:28 -0800

    adding the package controller

---
+++ packages/thedaywefightback/controller.php
@@ -0,0 +1,31 @@
+<?php
+class ThedaywefightbackPackage extends Package {
+
+	protected $pkgHandle = 'thedaywefightback';
+	protected $appVersionRequired = '5.5.0';
+	protected $pkgVersion = '0.9';
+	
+	public function getPackageDescription() {
+		return t("Displays a footer banner from thedaywefightback.org.");
+	}
+	
+	public function getPackageName() {
+		return t("The Day We Fight Back.");
+	}
+	
+	public function install() {
+		$pkg = parent::install();
+	}
+
+	public function on_start() {
+		$view = View::getInstance();
+		$view->addFooterItem('<!--[if !(lt IE 8)]><!-->
+   			<script type="text/javascript">
+   			var tdwfb_config = {
+				disableDate: false // If true, the banner shows even if the date is not yet 02/11/2014. Use for testing.
+  				};
+     			(function(){var e=document.createElement("script");e.type="text/javascript";e.async=true;e.src=document.location.protocol+"//d1agz031tafz8n.cloudfront.net/thedaywefightback.js/widget.min.js";var t=document.getElementsByTagName("script")[0];t.parentNode.insertBefore(e,t)})()
+   			</script>
+			<!--<![endif]-->');
+	}
+}
