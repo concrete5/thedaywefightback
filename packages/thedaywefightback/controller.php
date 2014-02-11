ca51c6f161397951e9967a4e6796cae18e960068
tylerryan <ryan@concrete5.org>
2014-02-10 21:37:31 -0800

    1.0 release

--- packages/thedaywefightback/controller.php
+++ packages/thedaywefightback/controller.php
@@ -6,7 +6,7 @@ class ThedaywefightbackPackage extends Package {
 
 	protected $pkgHandle = 'thedaywefightback';
 	protected $appVersionRequired = '5.4.1';
-	protected $pkgVersion = '0.9.2';
+	protected $pkgVersion = '0.1';
 
 	public function getPackageDescription() {
 		return t("Displays a footer banner from thedaywefightback.org.");
@@ -21,14 +21,14 @@ class ThedaywefightbackPackage extends Package {
 	}
 
 	public function on_start() {
-		if (strtotime('now') < strtotime('12:01am february 12th 2014') && !Loader::helper('fight_back', 'thedaywefightback')->inDashboard()){
+		if (strtotime('now') < strtotime('12:01am february 13th 2014') && !Loader::helper('fight_back', 'thedaywefightback')->inDashboard()){
 			$view = View::getInstance();
 			$view->addHeaderItem(
 "<!--[if !(lte IE 8)]><!--> 
 <script type='text/javascript'> 
 	var tdwfb_config = {
 		greeting: 'Dear Internet', // Sets the salutation at the top left, will be followed by a comma
-		disableDate: true, // If true, the banner shows even if the date is not yet 02/11/2014
+		disableDate: false, // If true, the banner shows even if the date is not yet 02/11/2014
 		callOnly: false // If true, the banner only displays a form for calling congress
 	};
 	(function(){
